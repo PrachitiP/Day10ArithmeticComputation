87780e8a6e529a615402cea548353ebbbe432b2e
PrachitiP <patilprachiti35@gmail.com>
2020-08-29 00:01:40 +0530

    Take Inputs

--- ArithmeticComputation/ArithComputation.sh
+++ ArithmeticComputation/ArithComputation.sh
@@ -2,3 +2,14 @@
 
 echo "Welcome to Arithmetic Computations and Sorting"
 
+# UseCase 1 => taking 3 numbers as input
+
+read -p "enter first number:" Number1
+read -p "enter second number:" Number2
+read -p "enter third number:" Number3
+
+echo "entered numbers are:"
+
+echo "first number:"$Number1
+echo "second number:"$Number2
+echo "third number:"$Number3
