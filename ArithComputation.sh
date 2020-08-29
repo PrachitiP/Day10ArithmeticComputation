65e4fb5c4c3af2d794480d2dd4903a275f5bf6d7
PrachitiP <patilprachiti35@gmail.com>
2020-08-29 12:52:58 +0530

    Sort Results in Descending Order

--- ArithComputation.sh
+++ ArithComputation.sh
@@ -56,3 +56,19 @@ done
 
 #printing array elements
 echo "Array elements are:" ${Array[@]}
+
+# Usecase 8 => Sort in Ascending Order
+
+for((i=0;i<4;i++))
+do
+	for((j = 0; j<4-i-1; j++))
+	do
+		if [ ${Array[j]} -gt ${Array[$((j+1))]} ]
+		then
+			temp=${Array[j]}
+			Array[$j]=${Array[$((j+1))]}
+			Array[$((j+1))]=$temp
+		fi
+	done
+done
+echo "Sorted Array in ascending order:" ${Array[@]}
